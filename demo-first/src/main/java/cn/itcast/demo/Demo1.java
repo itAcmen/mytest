9b627367c0059664e552809c83789d19e06139c1
wjb <625233052@qq.com>
2018-08-27 17:22:19 +0800

    n次

--- demo-first/src/main/java/cn/itcast/demo/Demo1.java
+++ demo-first/src/main/java/cn/itcast/demo/Demo1.java
@@ -3,6 +3,5 @@ package cn.itcast.demo;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("你好111111111111111111111111");
-		  System.out.println("你好11111112222222222222222111111111111");
     }
 }
